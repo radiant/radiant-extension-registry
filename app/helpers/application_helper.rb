10eefc17a564e22d5fc3d612210255b10668c425
Sean Cribbs <seancribbs@gmail.com>
2008-07-15 08:37:08 -0400

    Beginning a lighter, more friendly design, inspired by the greys of GitHub.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'digest/md5'
 module ApplicationHelper
+  def gravatar_url(email, size=80)
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}&d=wavatar"
+  end
 end
