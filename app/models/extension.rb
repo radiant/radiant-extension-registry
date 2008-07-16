820044ccccba126a6a3c53855d7765852f9e19ad
Sean Cribbs <seancribbs@gmail.com>
2008-07-16 07:54:09 -0400

    Fix nil uniqueness bug on extension.

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -5,8 +5,8 @@ class Extension < ActiveRecord::Base
   validates_uniqueness_of :name, :allow_nil => true
   validates_presence_of :repository_url, :if => proc {|e| e.download_url.blank? }, :message => "required unless download URL given"
   validates_presence_of :download_url, :if => proc {|e| e.repository_url.blank? }, :message => "required unless repository URL given"
-  validates_uniqueness_of :repository_url, :allow_nil => true
-  validates_uniqueness_of :download_url, :allow_nil => true
+  validates_uniqueness_of :repository_url, :if => :repository_url?
+  validates_uniqueness_of :download_url, :if => :download_url?
   validates_presence_of :author_id
   validates_presence_of :install_type
   
