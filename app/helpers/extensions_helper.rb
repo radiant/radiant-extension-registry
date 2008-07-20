21cbde1d710c7873c217175ecbf69db1cc29656d
Sean Cribbs <seancribbs@gmail.com>
2008-07-20 11:51:00 -0400

    Add additional installation types.

--- app/helpers/extensions_helper.rb
+++ app/helpers/extensions_helper.rb
@@ -1,2 +1,13 @@
 module ExtensionsHelper
+  def installation_types_for_select
+    [
+      'Git',
+      'Subversion',
+      'Gem',
+      ['Tar (.tar)', 'Tarball'],
+      ['GZip (.tar.gz, .tgz)', 'Gzip'],
+      ['BZip2 (.tar.bz2)', 'Bzip2'],
+      ['Zip (.zip)', 'Zip']
+    ]
+  end
 end
