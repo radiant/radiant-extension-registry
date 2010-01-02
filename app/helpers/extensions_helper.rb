82c704155397ed24e202ad6ad01aacdc63325fcf
John W. Long <me@johnwlong.com>
2010-01-02 00:46:46 -0500

    reformatted extension index in preparation for the new styles

--- app/helpers/extensions_helper.rb
+++ app/helpers/extensions_helper.rb
@@ -1,10 +1,17 @@
 module ExtensionsHelper
+  
+  def short_description(description)
+    first_line = description.split("\n\n").first
+    sanitize(textilize(truncate(first_line, :length => 200)))
+  end
+  
   def repository_types_for_select
     [
       'Git',
       'Subversion'
     ]
   end
+  
   def download_types_for_select
     [
       'Gem',
