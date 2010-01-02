40821ade86d1cfa29d819d40dab5d77c440b1fa3
John W. Long <me@johnwlong.com>
2010-01-02 02:10:37 -0500

    reformatted author index in preparation for new look

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,26 @@
 require 'digest/md5'
 module ApplicationHelper
   
-  def gravatar_url(email, size=80)
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?s=#{size}&d=wavatar"
+  # Returns a Gravatar URL associated with the email parameter.
+  # See: http://douglasfshearer.com/blog/gravatar-for-ruby-and-ruby-on-rails
+  def gravatar_url(email, options={})
+    # Default to highest rating. Rating can be one of G, PG, R X.
+    options[:rating] ||= "G"
+  
+    # Default size of the image.
+    options[:size] ||= "80"
+  
+    # Default image url to be used when no gravatar is found
+    # or when an image exceeds the rating parameter.
+    options[:default] ||= "http://#{request.host}:#{request.port}/images/avatar_#{([options[:size].to_i] * 2).join('x')}.png"
+  
+    # Build the Gravatar url.
+    url = 'http://www.gravatar.com/avatar.php?'
+    url << "gravatar_id=#{Digest::MD5.new.update(email)}" 
+    url << "&rating=#{options[:rating]}" if options[:rating]
+    url << "&size=#{options[:size]}" if options[:size]
+    url << "&default=#{options[:default]}" if options[:default]
+    url
   end
   
   def content_for?(symbol)
