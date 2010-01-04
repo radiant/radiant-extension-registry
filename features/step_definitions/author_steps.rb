0ed206889b2993d861f965765a73ba96489c6b40
John W. Long <me@johnwlong.com>
2010-01-04 18:48:31 -0500

    first pass at home page

---
+++ features/step_definitions/author_steps.rb
@@ -0,0 +1,23 @@
+def create_author(name, attributes = {})
+  login = name.downcase.gsub(" ", "_")
+  attributes = {
+    :first_name => name,
+    :login => login,
+    :email => "#{login}@example.com",
+    :password => "test",
+    :password_confirmation => "test",
+    :available_for_hire => [true, false].rand
+  }.merge(attributes)
+  Author.create!(attributes)
+end
+
+Given /^an author "([^\"]*)"$/ do |name|
+  create_author(name)
+end
+
+Given /^authors (".*")$/ do |authors|
+  authors = authors.gsub(/[" ]|and/, '').split(",")
+  authors.each do |name|
+    create_author(name)
+  end
+end
