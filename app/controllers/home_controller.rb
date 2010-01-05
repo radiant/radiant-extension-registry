34f6c52b20d6dbd06c9545d3b46f7575e229b3eb
John W. Long <me@johnwlong.com>
2010-01-05 16:15:27 -0500

    only show extension authors on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 6)
-    @authors = Author.find(:all, :order => "RANDOM()", :limit => 5)
+    @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 5)
+    @authors = Author.find(:all, :conditions => ["extensions_count > 0"], :order => "RANDOM()", :limit => 5)
   end
 end
