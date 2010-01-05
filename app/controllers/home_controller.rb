5dd524f0c5131ecbc4d741ab76045659a69cdb5e
John W. Long <me@johnwlong.com>
2010-01-04 20:52:26 -0500

    random authors on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 7)
-    @authors = Author.find(:all, :order => "created_at DESC", :limit => 5)
+    @authors = Author.find(:all, :order => "RANDOM()", :limit => 5)
   end
 end
