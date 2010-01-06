51b4c56e903337bcc45ed79e670d3c828ff993ab
John W. Long <me@johnwlong.com>
2010-01-05 19:40:18 -0500

    font tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 5)
+    @extensions = Extension.find(:all, :order => "created_at DESC", :limit => 4)
     @authors = Author.find(:all, :conditions => ["extensions_count > 0"], :order => "RANDOM()", :limit => 5)
   end
 end
