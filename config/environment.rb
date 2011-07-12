a7033bd0fe3de5cda4ea3a4254466169abffee37
Andrew vonderLuft <avonderluft@avlux.net>
2011-07-12 09:59:06 -0700

    i18n 5.0 does not properly expand {{}} variables.  Specify version 0.4.2

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   config.gem "paperclip", :version => ">=2.3.1.1"
   config.gem "will_paginate", :version => "~> 2.3.11", :source => "http://gemcutter.org"
   config.gem "thinking-sphinx", :version => "1.3.18", :lib => "thinking_sphinx", :source => "http://gemcutter.org"
+  config.gem "i18n", :version => "0.4.2"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
